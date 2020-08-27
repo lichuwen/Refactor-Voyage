65716f6d305064e896252a99e53b0e8632e7db77
lichuwen <545759585@qq.com>
2020-08-27 22:24:44 +0800

    [karen] test:add voyageRisk testCase1 of rankTest

--- test/rankTest.js
+++ test/rankTest.js
@@ -1,1 +1,34 @@
+const rankTest = require('ava');
+const {voyageRisk} = require("../src/rank")
 
+const voyage = {
+  zone: 'west-indies',
+  length: 10,
+};
+const history = [
+  {
+    zone: 'east-indies',
+    profit: 5,
+  },{
+    zone: 'west-indies',
+    profit: 15,
+  },{
+    zone: 'china',
+    profit: -2,
+  },
+  {
+    zone: 'west-africa',
+    profit: 7,
+  },
+];
+
+rankTest('rank voyageRisk-case1. voyage length more than 4 and less than 8', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 5,
+  };
+
+  const result = voyageRisk(voyage);
+  const expect = 3;
+  t.is(result, expect);
+});
