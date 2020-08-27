37144f333d0f836eea85f4e2b9ddfeff218d8b5b
lichuwen <545759585@qq.com>
2020-08-27 22:37:22 +0800

    [karen] test:add voyageRisk testCase1 of rankTest

--- test/rankTest.js
+++ test/rankTest.js
@@ -32,3 +32,14 @@ rankTest('rank voyageRisk-case1. voyage length more than 4 and less than 8', t =
   const expect = 3;
   t.is(result, expect);
 });
+
+rankTest('rank voyageRisk-case2. voyage length more than 8 and not include zone', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 9,
+  };
+
+  const result = voyageRisk(voyage);
+  const expect = 4;
+  t.is(result, expect);
+});
