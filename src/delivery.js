fb51cc46953f952ca7e72b3e27cd1dad505d71df
lichuwen <545759585@qq.com>
2020-08-28 20:30:27 +0800

    [karen] refactor: Use ternary expressions instead of 'if' in getDeliveryTime

--- src/delivery.js
+++ src/delivery.js
@@ -13,28 +13,15 @@ function getStates(state1,state2,state3) {
 }
 
 function getDeliveryTimeByTrue(anOrder) {
-  if(getStates(MASSACHUSETTS,CONNECTICUT,null).includes(anOrder.deliveryState)) {
-     return 1;
-  }
-  if(getStates(NEW_YORK,NEW_HAMPSHIRE,null).includes(anOrder.deliveryState)) {
-    return 2;
-  }
-  return 3;
+  return getStates(MASSACHUSETTS, CONNECTICUT, null).includes(anOrder.deliveryState) ? 1 : (getStates(NEW_YORK, NEW_HAMPSHIRE, null).includes(anOrder.deliveryState)) ? 2 : 3;
 }
 
 function getDeliveryTimeByFalse(anOrder) {
-  if(getStates(MASSACHUSETTS,CONNECTICUT,NEW_YORK).includes(anOrder.deliveryState)) {
-    return 2;
-  }
-  if(getStates(MAINE,NEW_HAMPSHIRE,null).includes(anOrder.deliveryState)) {
-    return 3;
-  }
-  return 4;
+  return getStates(MASSACHUSETTS, CONNECTICUT, NEW_YORK).includes(anOrder.deliveryState) ? 2 : getStates(MAINE, NEW_HAMPSHIRE, null).includes(anOrder.deliveryState) ? 3 : 4;
 }
 
 function deliveryDate(anOrder) {
   return anOrder.isRush ? anOrder.placedOn.plusDays(1 + getDeliveryTimeByTrue(anOrder)) : anOrder.placedOn.plusDays(2 + getDeliveryTimeByFalse(anOrder));
-  ;
 }
 
 module.exports = {deliveryDate}
