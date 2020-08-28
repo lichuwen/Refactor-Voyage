b6548fc9b5c64323db8fa2d79a102f352946ab27
lichuwen <545759585@qq.com>
2020-08-28 18:34:05 +0800

    [karen] refactor:extract getDeliveryTimeByFalse of deliveryTest.js

--- src/delivery.js
+++ src/delivery.js
@@ -12,7 +12,7 @@ function getStates(state1,state2,state3) {
   ]
 }
 
-function getDeliveryTime(anOrder) {
+function getDeliveryTimeByTrue(anOrder) {
   if(getStates(MASSACHUSETTS,CONNECTICUT,null).includes(anOrder.deliveryState)) {
      return 1;
   }
@@ -22,28 +22,22 @@ function getDeliveryTime(anOrder) {
   return 3;
 }
 
+function getDeliveryTimeByFalse(anOrder) {
+  if(getStates(MASSACHUSETTS,CONNECTICUT,NEW_YORK).includes(anOrder.deliveryState)) {
+    return 2;
+  }
+  if(getStates(MAINE,NEW_HAMPSHIRE,null).includes(anOrder.deliveryState)) {
+    return 3;
+  }
+  return 4;
+}
+
 function deliveryDate (anOrder, isRush) {
   if (isRush) {
-    return anOrder.placedOn.plusDays(1 + getDeliveryTime(anOrder));
+    return anOrder.placedOn.plusDays(1 + getDeliveryTimeByTrue(anOrder));
   }
   else {
-    let deliveryTime;
-    if ([
-      MASSACHUSETTS,
-      CONNECTICUT,
-      NEW_YORK,
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    }
-    else if ([
-      MAINE,
-      NEW_HAMPSHIRE,
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 3;
-    } else {
-      deliveryTime = 4;
-    }
-    return anOrder.placedOn.plusDays(2 + deliveryTime);
+    return anOrder.placedOn.plusDays(2 + getDeliveryTimeByFalse(anOrder));
   }
 }
 
