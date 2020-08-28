29c7546cb9f3bc277e99937cbbc7097fcd64bdfb
lichuwen <545759585@qq.com>
2020-08-28 08:37:58 +0800

    [karen] test:add deliveryDate-case2 of deliveryTest

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -21,3 +21,19 @@ deliveryTest('delivery deliveryDate-case1. Order MA and rush is true', t => {
     const result = deliveryDate(anOrder, isRush);
     t.is(result, except);
 })
+
+deliveryTest('delivery deliveryDate-case2. Order NY and rush is true', t => {
+    const anOrder = {
+        "deliveryState": 'NY',
+        "placedOn": {
+            "plusDays": function (deliveryTime) {
+                plusDays.setDate(today.getDate() + deliveryTime);
+                return plusDays.toLocaleDateString();
+            }
+        }
+    }
+    plusDays.setDate(today.getDate() + 3);
+    const except = plusDays.toLocaleDateString();
+    const result = deliveryDate(anOrder, isRush);
+    t.is(result, except);
+})
