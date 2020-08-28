0d49eee2b0f0fb30b6705b08fd698ce948ffeec6
lichuwen <545759585@qq.com>
2020-08-28 17:46:17 +0800

    [karen] test:add deliveryDate-case3 of deliveryTest.js

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -2,9 +2,9 @@ const deliveryTest = require('ava')
 const {deliveryDate} = require('../src/delivery');
 
 let isRush = true;
-let today = new Date();
-let plusDays = new Date();
-let singlePlusDay = new Date();
+const today = new Date();
+let plusDay = new Date();
+const plusDays = new Date();
 
 let anOrder = {
     "deliveryState": 'MA',
@@ -17,48 +17,25 @@ let anOrder = {
 }
 
 deliveryTest('delivery deliveryDate-case1. Order MA and rush is true', t => {
-    singlePlusDay.setDate(today.getDate() + 2);
-    const except = singlePlusDay.toLocaleDateString();
+    plusDay.setDate(today.getDate() + 2);
+    const except = plusDay.toLocaleDateString();
     const result = deliveryDate(anOrder, isRush);
     t.is(result, except);
 })
 
 deliveryTest('delivery deliveryDate-case2. Order NY and rush is true', t => {
-    const anOrder = {
-        "deliveryState": 'NY',
-        "placedOn": {
-            "plusDays": function (deliveryTime) {
-                plusDays.setDate(today.getDate() + deliveryTime);
-                return plusDays.toLocaleDateString();
-            }
-        }
-    }
-    singlePlusDay.setDate(today.getDate() + 3);
-    const except = singlePlusDay.toLocaleDateString();
+    anOrder.deliveryState = 'NY';
+    plusDay.setDate(today.getDate() + 3);
+    const except = plusDay.toLocaleDateString();
     const result = deliveryDate(anOrder, isRush);
     t.is(result, except);
 })
 
 deliveryTest('delivery deliveryDate-case3. Order CW and rush is true', t => {
-    anOrder = {
-        "deliveryState": 'CW',
-        "placedOn": {
-            "plusDays": function (deliveryTime) {
-                plusDays.setDate(today.getDate() + deliveryTime);
-                return plusDays.toLocaleDateString();
-            }
-        }
-    }
-    singlePlusDay.setDate(today.getDate() + 4);
-    const except = singlePlusDay.toLocaleDateString();
+    anOrder.deliveryState = 'CW'
+    plusDay.setDate(today.getDate() + 4);
+    const except = plusDay.toLocaleDateString();
     const result = deliveryDate(anOrder, isRush);
     t.is(result, except);
 })
 
-// deliveryTest('delivery deliveryDate-case4. Order MA and rush is false', t => {
-//     isRush = false;
-//     plusDays.setDate(today.getDate() + 4);
-//     const except = plusDays.toLocaleDateString();
-//     const result = deliveryDate(anOrder, isRush);
-//     t.is(result, except);
-// })
